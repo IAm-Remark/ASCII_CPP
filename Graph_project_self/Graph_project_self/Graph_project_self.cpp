fbef074bdb6508e31bfdccf1eccbdf6e57348eb5
IAm_Remark <gangamstul4@gmail.com>
2023-09-29 16:29:41 +0300

    Added opencv

--- Graph_project_self/Graph_project_self/Graph_project_self.cpp
+++ Graph_project_self/Graph_project_self/Graph_project_self.cpp
@@ -1,7 +1,30 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include <opencv2\highgui.hpp>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cv::Mat img = cv::imread("D:/projects/3D_Graph/Graph_project_self/res/pics/1.jpg");
+    cv::Mat img1 = cv::imread("D:/projects/3D_Graph/Graph_project_self/res/pics/2.jpg");
+
+    cv::imshow("window", img1);
+    cv::imshow("window_second", img1);
+
+    printf("%d\n", img1.size().height);
+    printf("%d\n", img1.size().width);
+    cv::waitKey(0);
+}
+
+int draw()
+{
+    return 0;
+}
+
+int convert()
+{
+    return 0;
+}
+
+int draw_converted()
+{
+    return 0;
 }
